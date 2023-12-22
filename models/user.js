79e6776fd7914e772a680efa55440d407d9ba8b9
Chandra Shekar <mchandrashekar842@gmail.com>
2023-12-22 17:12:49 +0530

    updated files

--- models/user.js
+++ models/user.js
@@ -15,26 +15,45 @@ export class User {
     }
 
     addToCart(product) {
-        const cartProductIndex = this.cart.items.findIndex(cp =>{return cp.productId.toString() === product._id.toString()})
+        const cartProductIndex = this.cart.items.findIndex(cp => { return cp.productId.toString() === product._id.toString() })
         let newQuantity = 1;
         let updatedCartItems = [...this.cart.items]
-        if(cartProductIndex >= 0) {
+        if (cartProductIndex >= 0) {
             newQuantity = this.cart.items[cartProductIndex].quantity + 1;
             updatedCartItems[cartProductIndex].quantity = newQuantity;
         } else {
-            updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quanitity: newQuantity})
+            updatedCartItems.push({ productId: new mongodb.ObjectId(product._id), quantity: newQuantity })
         }
-        const updatedCart = {items: updatedCartItems}
+        const updatedCart = { items: updatedCartItems }
         const db = getDb()
-        return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: { cart: updatedCart }})
+        return db.collection('users').updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: updatedCart } })
+    }
+
+    getCart() {
+        const db = getDb()
+        const productIds = this.cart.items.map(i => {
+            return i.productId
+        })
+        return db.collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => {
+                return products.map(p => {
+                    return {
+                        ...p,
+                        quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === p._id.toString()
+                        }).quantity
+                    }
+                })
+            })
     }
 
     static findById(userId) {
         const db = getDb()
         return db.collection('users')
-            .findOne({_id : new mongodb.ObjectId(userId)})
+            .findOne({ _id: new mongodb.ObjectId(userId) })
             .then(user => {
-                console.log(user)
                 return user
             })
             .catch(err => console.log(err))
