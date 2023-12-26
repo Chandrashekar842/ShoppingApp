5845117831baece47e97aeda7a9c259d29a4e46f
Chandra Shekar <mchandrashekar842@gmail.com>
2023-12-26 12:31:30 +0530

    models are redefined using mongoose

--- models/user.js
+++ models/user.js
@@ -1,3 +1,27 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }, 
+    cart: {
+        items: [{
+            productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+            quantity: { type: Number, required: true }
+        }]
+    }
+})
+
+export const User = mongoose.model('User', userSchema)
+
+
 // import { getDb } from "../util/database.js"
 // import mongodb from 'mongodb'
 
