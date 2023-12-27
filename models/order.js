8880259c27cce38678519723a407af5a88cf1383
Chandra Shekar <mchandrashekar842@gmail.com>
2023-12-27 12:30:19 +0530

    order functionality added

---
+++ models/order.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const orderSchema = new Schema({
+    products: [{
+        product: { type: Object, required: true},
+        quantity: { type: Number, required: true}
+    }],
+    user: {
+        userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        name: { type: String, required: true }
+    }
+})
+
+export const Order = mongoose.model('Order', orderSchema)
+
