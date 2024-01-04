394128d109f9a028b822bfae3b42c2fb1942d1d3
Chandra Shekar <mchandrashekar842@gmail.com>
2024-01-04 12:44:59 +0530

    sample authentication added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,25 @@
+import { User } from "../models/user.js"
+
 export const getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: false
+    })
+}
+
+export const postLogin = (req, res, next) => {
+    User.findById('658a74ed79fdfbae08327788')
+        .then(user => {
+            req.session.isLoggedIn = true
+            req.session.user = user
+            res.redirect('/')
+        })
+        .catch(err => console.log(err))
+}
+
+export const postLogOut = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect('/')
     })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
-import { getLogin } from '../controllers/auth.js'
+import { getLogin, postLogin, postLogOut } from '../controllers/auth.js'
 
 const authrouter = express.Router()
 
 authrouter.get('/login', getLogin)
+
+authrouter.post('/login', postLogin)
+
+authrouter.post('/logout', postLogOut)
 
 export default authrouter
